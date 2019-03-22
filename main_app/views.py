fc0c8e1239ddb2dcb317ab147268e794b0daf0c5
Eliana Levenson <elianalevenson@Elianas-MacBook-Air.local>
2019-03-21 21:58:04 -0700

    css updates & touchups

--- main_app/views.py
+++ main_app/views.py
@@ -10,6 +10,7 @@ from django.views.generic import ListView, DetailView
 from django.contrib.auth import authenticate, login
 import os
 import requests
+import random
 from datetime import datetime, date
 from .models import Trip, Hotel, Flight, User, Suitcase
 from .forms import LuggageForm
@@ -76,7 +77,7 @@ def destinations(request):
         'budget': budget, 'origin': origin, 
         'departure_date':d_date, 
         "trip": trip,
-        'iata': res
+        'iata': res       
         })
     except: 
         return render (request, 'error.html')
